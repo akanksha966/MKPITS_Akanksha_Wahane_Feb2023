122f2aa2ac533a0c9705dc7c12ddde5f74db719f
Akanksha <wahaneakansha2@gmail.com>
2023-03-24 18:02:20 +0530

    24-3-23

---
+++ csharp/prime/prime/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace prime
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int num = 6;
+
+            int counter;
+            int flag = 0;
+            for (counter = 2; counter < num; counter++)
+            {
+                if (num % counter == 0)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+                if(flag==0)
+                {
+                    Console.WriteLine("it is a prime no");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("it is not prime no");
+                    Console.ReadLine();
+
+                }
+                
+            }
+        }
+    }
+
