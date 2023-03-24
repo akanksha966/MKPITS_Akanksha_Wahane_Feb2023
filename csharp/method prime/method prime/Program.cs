1fcd3107a0fe492e1ac13e0a93ae76c97d82c993
Akanksha <wahaneakansha2@gmail.com>
2023-03-24 10:02:39 +0530

    24-3-23

---
+++ csharp/method prime/method prime/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace factorial_one_parameter
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+
+            Console.WriteLine("enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            primeno(num);
+
+            //Console.WriteLine("prime no" + result);
+            Console.ReadLine();
+        }
+
+       
+        static void primeno(int n1)
+        {
+            int counter = 2;
+            while(counter<n1)
+            {
+                if(n1%2==0)
+                {
+                    Console.WriteLine("it is not prime no");
+                    break;
+                    
+                }
+                else
+                {
+                    Console.WriteLine("it is prime no");
+                    break;
+                    
+
+                }
+                counter = counter + 1;
+
+
+
+
+            }
+            
+
+
+
+            
+
+        }
+
+    }
+}
