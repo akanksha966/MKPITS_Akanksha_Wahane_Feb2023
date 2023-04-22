b2882be8d7bbee579256d7d1b72f2c5ef1e6bca3
Akanksha <wahaneakansha2@gmail.com>
2023-04-22 15:33:01 +0530

    method parameter and return value

---
+++ csharp/method with parameter and return value/method with parameter and return value/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_with_parameter_and_return_value
+{
+    class addition
+    {
+        public int add(int num1,int num2)
+        {
+            int result=num1 + num2;
+            return result;
+
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            addition s = new addition();
+            int a = 10;
+            int b = 20;
+            int c=s.add(a,b);
+            Console.WriteLine("addition is "+c);
+            Console.ReadLine();
+
+        }
+    }
+}
