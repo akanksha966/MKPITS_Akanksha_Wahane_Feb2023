febfa6bc35b036c8d0560468e8e67c55c3e05a83
Akanksha <wahaneakansha2@gmail.com>
2023-04-24 11:12:43 +0530

    added program

---
+++ csharp/using non static example/using non static example/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace using_non_static_example
+{
+    class print
+    {
+        
+        public void print2(int num1,int num2)
+        {
+            int result=num1 + num2;
+            Console.WriteLine("result is "+result);
+            Console.ReadLine();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            print s = new print();
+            s.print2 (3,2);
+        }
+    }
+}
