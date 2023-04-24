1dd546dc70133698e7f67e4348b24ec6c6743a6f
Akanksha <wahaneakansha2@gmail.com>
2023-04-24 13:02:53 +0530

    mmm

---
+++ csharp/default/default/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_ignore
+{
+    class calculate
+    {
+        public calculate()
+        {
+            Console.WriteLine("hello world");
+            Console.ReadLine();
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculate stud = new calculate();
+        }
+    }
+}
