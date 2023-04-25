2e74f638e5e5c245458bd2ce699c689b7c95ecba
Akanksha <wahaneakansha2@gmail.com>
2023-04-25 22:19:09 +0530

    private access modifier

---
+++ csharp/internal acceess modifier/internal acceess modifier/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace internal_acceess_modifier
+{
+    class animal
+    {
+        internal string name;
+        internal void dog()
+        {
+            Console.WriteLine(" name is " + name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            animal dog = new animal();
+            dog.name = "Dog";
+            dog.dog();  
+            Console.ReadLine(); 
+        }
+    }
+}
