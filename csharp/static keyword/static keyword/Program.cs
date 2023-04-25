0d99d2eabffc75ecacfbeac59ef8e2f81759d9b7
Akanksha <wahaneakansha2@gmail.com>
2023-04-25 12:48:31 +0530

    static keyword example

---
+++ csharp/static keyword/static keyword/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_keyword
+{
+    class account
+    {
+        public int actno;
+        public string name;
+        public static float rof=4.5f;
+            //rate of interest
+          public account(int actno,string name)//method
+        {
+            this.actno = actno;
+            this.name=name;
+        }
+        public void display()
+        {
+            Console.WriteLine("account no us " + actno);
+            Console.WriteLine("account name " + name);
+            Console.WriteLine("rate of interest is " + rof);
+
+
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            account act=new account(1234,"ashu");
+            act.display();
+            account act2 = new account( 22223,"asjs");
+            act.display();
+            Console.ReadLine();
+        }
+    }
+}
