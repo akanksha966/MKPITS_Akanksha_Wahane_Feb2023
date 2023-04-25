ca55941b51314dd0a2be4a41855e08e2e7ac893d
Akanksha <wahaneakansha2@gmail.com>
2023-04-25 16:21:23 +0530

    static constructor

---
+++ csharp/static constructor/static constructor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_constructor
+{
+    public class calculate
+    {
+        public static float pi;
+        static calculate()//static constructor
+        {
+            pi = 3.14f;
+            Console.WriteLine("static constructor called");
+        }
+        public void display()
+        {
+            Console.WriteLine("rate of interest is " + pi);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculate a1 = new calculate();//static constructor yek he bar call hota hai
+                                           //chahe kitne bhi object bne ho
+            a1.display();
+            Console.WriteLine("=========");
+
+            calculate a2 = new calculate();
+            a2.display();
+            Console.ReadLine();
+        }
+    }
+}
--- csharp/static keyword/static keyword/Program.cs
+++ csharp/static keyword/static keyword/Program.cs
@@ -11,14 +11,11 @@ namespace static_keyword
         public int actno;
         public string name;
         public static float rof=4.5f;
-        public static int count;
-
             //rate of interest
           public account(int actno,string name)//method
         {
             this.actno = actno;
             this.name=name;
-            count++;
         }
         public void display()
         {
@@ -36,13 +33,9 @@ namespace static_keyword
         static void Main(string[] args)
         {
             account act=new account(1234,"ashu");
-            account .rof = 33.4f;
-
             act.display();
             account act2 = new account( 22223,"asjs");
-            account .rof = 33.4f;
             act.display();
-            Console.WriteLine("no of object "+account.count);
             Console.ReadLine();
         }
     }
