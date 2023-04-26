92a240f793f7e8ea44dcc8bba09ed1b0369020ab
Akanksha <wahaneakansha2@gmail.com>
2023-04-26 16:16:17 +0530

    try catch and throw example

---
+++ csharp/throw block/throw block/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace throw_block
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            try
+            {
+                Console.WriteLine("enter age ");
+                age = Convert.ToInt32(Console.ReadLine());
+                if(age<=0 || age>=100)
+                {
+                    throw new Exception("enter proper age");
+                }
+                else
+                {
+                    Console.WriteLine("your age is " + age);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.WriteLine("bye");
+            Console.ReadLine();
+        }
+    }
+}
