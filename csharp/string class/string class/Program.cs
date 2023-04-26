451c69e2a3637b5443a2fd71e2f4a4ad4b8c144d
Akanksha <wahaneakansha2@gmail.com>
2023-04-26 18:01:21 +0530

    string class

---
+++ csharp/string class/string class/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "hello";//using string class program 
+            Console.WriteLine(s1);
+            char[] s = { 'h', 'i', 'i', 'i' };
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
