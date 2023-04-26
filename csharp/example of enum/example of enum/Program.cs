34ebc829621d33811b8a3b9d0816592fecb800db
Akanksha <wahaneakansha2@gmail.com>
2023-04-26 12:27:53 +0530

    enum datatype example

---
+++ csharp/example of enum/example of enum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_of_enum
+{
+    enum month
+    {
+        jan,feb,march
+        
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("jan is vale " +(int)month.jan);
+            Console.WriteLine("jan is vale " + (int)month.feb);
+            Console.WriteLine("jan is vale " +Convert.ToInt32(month.march));
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
