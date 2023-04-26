a3e54dd631512afa20dbbbbb71e994413733ff80
Akanksha <wahaneakansha2@gmail.com>
2023-04-26 13:04:38 +0530

    static class example revision

---
+++ csharp/revision of static classs/revision of static classs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace revision_of_static_classs
+{
+    public static class animal
+    {
+        public static void display()
+        {
+            Console.WriteLine("hello static class");
+            Console.ReadLine();
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          animal.display(); 
+            
+        }
+    }
+}
