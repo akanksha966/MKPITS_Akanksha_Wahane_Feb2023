581e4a28f889bfed929b1ce484f2d1b3363e9db2
Akanksha <wahaneakansha2@gmail.com>
2023-04-26 12:49:54 +0530

    enum datatype example of circle and square

---
+++ csharp/another exap of enum data type/another exap of enum data type/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace another_exap_of_enum_data_type
+{
+    public class perimetr
+    {
+        public enum shape
+        {
+            circle,square
+        }
+        public void peri(int val,shape s1)//checking for shape to be circle
+
+        {
+            if(s1==0)
+            {
+                Console.WriteLine(" circle is " + 2 * 3.14 * val);
+            }
+            else
+            {
+                Console.WriteLine(" circle is " + 4*val);
+            }
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            perimetr a1= new perimetr();
+            a1.peri(3,perimetr.shape.circle);
+            a1.peri(3, perimetr.shape.square);
+            Console.ReadLine(); 
+
+
+
+
+        }
+    }
+}
