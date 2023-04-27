edf466a0ba0e20b1a70a055e515feea23cc59b42
Akanksha <wahaneakansha2@gmail.com>
2023-04-27 12:42:19 +0530

    string compare method

---
+++ csharp/string compare method/string compare method/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_compare_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "hello";
+            string s2 = "hello1";
+            int res=string.Compare(s1, s2);
+            Console.WriteLine("string is "+res);
+            Console.ReadLine();
+        }
+    }
+}
