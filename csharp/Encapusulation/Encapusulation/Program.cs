e97bb1b0c2ab10082c71b6f60dc8ebfbc25813f5
Akanksha Wahane <wahaneakansha2@gmail.com>
2023-05-06 14:41:41 +0530

    encapsulation using method example

---
+++ csharp/Encapusulation/Encapusulation/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapusulation
+{
+    class person
+    {
+        string _name;
+        public void setter(string name)
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("name is required");
+            }
+            else
+            {
+                _name = name;
+
+            }
+            
+        }
+        public void getter() //name kabhi bhi blank nhi rhta esliye hmne encapsulation ka use kr ke variable vo 
+                             //private kiye aur method ko public kiye aur usme logical operation perfotm kiye
+        {
+            if (string.IsNullOrEmpty(_name))
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("your name is "+_name);
+            }
+            
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            person o = new person();
+            o.setter("ashu");
+            o.getter();
+            Console.ReadLine();
+        }
+    }
+}
