9dda245b198ac76785cf648252b4a5a0eda0e44d
Akanksha Wahane <wahaneakansha2@gmail.com>
2023-05-06 19:16:57 +0530

    properties example

---
+++ csharp/properties 1 example/properties 1 example/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace properties_1_example
+{
+    class Employee
+    {
+        private string _name;
+        public string name //property
+        {
+            set 
+            { 
+                this._name = value;
+            }
+            get
+            {
+                return this._name;
+            }
+        }
+
+        
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Employee emp = new Employee();
+            emp.name = "ashu";
+            Console.WriteLine("name is "+emp.name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ csharp/property example2/property example2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace properties_1_example
+{
+    class Employee
+    {
+        private string _name;
+        public string name //property
+        {
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+
+                    Console.WriteLine("name is required");
+                }
+                else
+                {
+                    this._name = value;
+                }
+            }
+
+            get
+            {
+                
+                
+
+
+             return this._name;
+                
+            }
+        }
+    
+
+
+
+
+
+
+
+
+        
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Employee emp = new Employee();
+                emp.name = "ashu";
+                Console.WriteLine("name is " + emp.name);
+                Console.ReadLine();
+            }
+        }
+    }
+}
