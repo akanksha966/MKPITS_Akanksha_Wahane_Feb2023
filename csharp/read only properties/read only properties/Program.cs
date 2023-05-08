a9abeb3256c949ee6043f3b691afc878f6cdacbf
Akanksha Wahane <wahaneakansha2@gmail.com>
2023-05-08 12:14:28 +0530

    read only properties

---
+++ csharp/read only properties/read only properties/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace read_only_properties
+{
+    class employee
+    {
+        private int subjecatmarks=90;
+        public int _subjecatmarks
+        {
+            get
+            {
+                return subjecatmarks;   
+            }
+            
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            employee employee = new employee();
+            Console.WriteLine("marks is " + employee._subjecatmarks);
+            Console.ReadLine();
+        }
+    }
+}
