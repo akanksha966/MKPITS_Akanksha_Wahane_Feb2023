1b1677647047c59e7886aa56be3a8155ff2a4953
Akanksha Wahane <wahaneakansha2@gmail.com>
2023-05-15 21:27:41 +0530

    windows questions practice

---
+++ csharp/example2 without class connection/example2 without class connection/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace example2_without_class_connection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string abc = "server=.\\sqlexpress;integrated security=true;database=abcd;";
+            SqlConnection c=new SqlConnection(abc);
+            try
+            {
+                c.Open();
+                label1.Text = "connection done";
+            }
+            catch (Exception ex) 
+            { 
+                label1.Text = ex.Message;   
+            }
+            finally { c.Close(); }
+        }
+    }
+}
---
+++ csharp/without cstatic classs connection/without cstatic classs connection/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace without_cstatic_classs_connection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string con = "server=.\\sqlexpres;integrated security=true;database=abcd;";
+            SqlConnection c = new SqlConnection(con);
+            try
+            {
+                c.Open();
+                label1.Text = "connect successfully";
+
+
+
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "not connect";
+            }
+            finally
+            { c.Close(); }
+        }
+    }
+}
