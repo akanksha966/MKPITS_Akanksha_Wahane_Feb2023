529be7e3e4ee221e833e87370a79ece68b53f8ae
Akanksha Wahane <wahaneakansha2@gmail.com>
2023-05-30 15:28:52 +0530

    vote asp

---
+++ csharp/WebApplication1/WebApplication1/vote.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class vote : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int age = Convert.ToInt32(TextBox1.Text);
+            if(age<18)
+            {
+                Label1.Text = "your not eligible for vote";
+            }
+            else
+            {
+                Label1.Text="your eligible for vote";
+            }
+        }
+    }
+}
