abc3a546da9eb21897670eee2f18e60b83e4541a
Akanksha Wahane <wahaneakansha2@gmail.com>
2023-07-10 15:42:02 +0530

    formcollection object

---
+++ csharp/formcollection_object/formcollection_object/Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+﻿using formcollection_object.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace formcollection_object.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult submit(FormCollection fc)
+        {
+            ViewBag.Id = fc["Id"];
+            ViewBag.Name = fc["Name"];
+            bool gender = Convert.ToBoolean(fc["gender"].Split(',')[0]);
+            ViewBag.gender = gender;
+
+            return View("Index");
+
+        }
+    }
+}
---
+++ csharp/formcollection_object/formcollection_object/Models/StudentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace formcollection_object.Models
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool gender { get; set; }    
+    }
+}
