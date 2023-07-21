5da77e000709c8089813eb9959a2a1fac34a594e
Akanksha Wahane <wahaneakansha2@gmail.com>
2023-07-21 18:49:43 +0530

    collection example using console application

---
+++ csharp/collectionExample1/collectionExample1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace collectionExample1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ArrayList list= new ArrayList();    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            list.Add(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Capacity" +list.Capacity);
+            MessageBox.Show("count" + list.Count);
+            StringBuilder sb = new StringBuilder();
+            foreach(object o in list)
+            {
+                sb.Append(o.ToString()+"\n");
+            }
+            label2.Text = sb.ToString();    
+        }
+    }
+}
---
+++ csharp/collectionex2/collectionex2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace collectionex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("bshu");
+            list.Add("ashu");
+
+            list.Add("rkanksha");
+            Console.WriteLine("array list collection without sorting");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+            ////////sorting code////////
+             list.Sort();
+            Console.WriteLine("array list collection withut sorting");
+            foreach(var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
